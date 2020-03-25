5edd3eeb5da0e4577cd39e3d6ea028c6002f49b0
Tomasz Sokół <tomasz.sokol@eo.pl>
2020-03-25 22:36:06 +0100

    initial commit

---
+++ src/test/java/pl/effectivesolutions/restendpointexample/RestEndpointExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.effectivesolutions.restendpointexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEndpointExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
